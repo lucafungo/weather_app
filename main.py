e1c50f2df3466f3f3e4193c994e7518d9896ec20
Luca <100308395+lucafungo@users.noreply.github.com>
2023-02-28 13:30:18 +0000

    comments added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+
 import requests
 
-
+# this funcion runs in case the city does not exist
 def city_not_found():
     print(f'Oops! this city does not exist!\n')
 
+# this function runs in case the city typed exixst and is found
 def city_found():
     r = requests.get(url).json()
     temp_in_kelvin = (r['main']['temp'])
@@ -14,13 +16,13 @@ def city_found():
     
     print(f"Looks like the weather in {city_input} is {description}\nThe temperature is {temp_in_celsius}°\nHumidity {humidity}%")
 
-
+# city input and URL handling
 api_key = '<PERSONAL APY KEY GOES HERE>'
 city_input = input('Please type a city: ')
 url = (f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&appid={api_key}")
 api_call = requests.get(url)
 
-
+# error handling
 if (api_call.status_code) == 200:
     city_found()
 elif (api_call.status_code) == 404:
